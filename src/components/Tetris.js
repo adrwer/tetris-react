48b10b27c8ac8eb5451a4a8ede5aa0685cfe780a
Adrian Werimo <adrwer7@gmail.com>
2023-08-05 14:23:27 +0300

    setup stage and tetrominos

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,3 +1,5 @@
+import { createStage } from "../gameHelpers";
+
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
@@ -5,7 +7,7 @@ import StartButton from "./StartButton";
 const Tetris = () => {
     return (
         <div>
-            <Stage />
+            <Stage stage={createStage()}/>
             <aside>
                 <div>
                     <Display text="Score" />
